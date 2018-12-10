f92ee4bf9c3e50262e021e3020eba894eae1e3d0
StefanRM <stefanmaftei@gmail.com>
2018-12-10 23:54:06 +0200

    [GUI] Added grid and fixed button bouncing

--- components/gui.py
+++ components/gui.py
@@ -3,13 +3,23 @@ import pygame
 DISP_TRACES_KEY = pygame.K_t
 DISP_GRID_KEY = pygame.K_g
 
+GRID_CELL_DIM = 20
+DIST_BETWEEN_GRIDS = 1
+TRACE_LINE_WIDTH = 2
+
 class Gui:
 
-    def __init__(self, period = 100, width = 500, height = 500, caption = "Air Simulator"):
+    def __init__(self, period = 100, width = 500, height = 500, caption = "Multi-Robot 2D Simulator", background_color = (0, 0, 0), grid_color = -1):
         self.period = period # miliseconds
         self.width = width
         self.height = height
         self.caption = caption
+        self.background_color = background_color
+        if grid_color == -1:
+            self.grid_color = (255 - background_color[0], 255 - background_color[1], 255 - background_color[2])
+        else:
+            self.grid_color = grid_color
+
         self.display_traces = False
         self.display_grid = False
         self.press_limiter_toggle_display_traces = False
@@ -40,9 +50,23 @@ class Gui:
                 self.press_limiter_toggle_display_traces = True
         else:
             self.press_limiter_toggle_display_traces = False
+        
+        if keys[DISP_GRID_KEY]:
+            if not self.press_limiter_toggle_display_gird:
+                self.toggle_display_grid()
+                self.press_limiter_toggle_display_gird = True
+        else:
+            self.press_limiter_toggle_display_gird = False
             
 
-        self.window.fill((0, 0, 0))
+        # drawing the grid
+        if self.display_grid:
+            self.window.fill(self.grid_color)
+            for i in range(0, self.width, DIST_BETWEEN_GRIDS + GRID_CELL_DIM):
+                for j in range(0, self.width, DIST_BETWEEN_GRIDS + GRID_CELL_DIM):
+                    pygame.draw.rect(self.window, self.background_color, (i, j, GRID_CELL_DIM, GRID_CELL_DIM))
+        else:
+            self.window.fill(self.background_color)
 
         robots = world.get_robots()
 
@@ -54,13 +78,14 @@ class Gui:
             width = dimension["width"]
             height = dimension["height"]
 
+            # drawing the traces
             if self.display_traces:
                 for i in range(len(trace) - 1):
                     (a, b) = trace[i]
                     (c, d) = trace[i + 1]
                     start = (a + width / 2, b + height / 2)
                     stop = (c + width / 2, d + height / 2)
-                    pygame.draw.line(self.window, robot.get_color(), start, stop)
+                    pygame.draw.line(self.window, robot.get_color(), start, stop, TRACE_LINE_WIDTH)
             
         for robot in robots:
             x = robot.get_coordinate_x()
